a9a526cc33c5916cfc47540f3b4aea3dbc34222f
BaekSeungYun <ryusim@cau.ac.kr>
2022-06-22 00:11:22 +0900

    Dashboard API (#18)
    * Cors 설정 + json format 변경
    
    * [feature/statistics] add Dashboard api

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from account.account import Account
 from advertisement.advertisement import Advertisement
 from metaverse.metaverse import Metaverse
+from dashboard.dashboard import Dashboard
 from dependency import configure
 from flask import Flask, jsonify, request, make_response
 from flask_injector import FlaskInjector
@@ -9,13 +10,14 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 api = Api(app, version='1.0', title='Meverty API', description='API description for Meverty Service')
 
 api.add_namespace(Metaverse, '/metaverse')
 api.add_namespace(Account, '/account')
 api.add_namespace(Advertisement, '/advertisement')
+api.add_namespace(Dashboard, '/dashboard')
 
 
 FlaskInjector(app=app, modules=[configure])
